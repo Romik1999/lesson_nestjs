a9b04e768e4aebdb7e83d7b9112725187c8a02bb
roman <usankin.roma@gmail.com>
2023-06-14 13:39:39 +0400

    add swagger, writing basic documentation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './modules/app/app.module';
 import {ConfigService} from "@nestjs/config";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService)
-    const port = configService.get('port');
+    const port = configService.get("port");
+
+    // напсиание документации swagger
+    const config = new DocumentBuilder()
+        .setTitle('Lesson API')
+        .setDescription('this api for lessons')
+        .setVersion('1.0')
+        .addTag('API')
+        .build()
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('api', app, document)
+
     await app.listen(port);
 }
 
