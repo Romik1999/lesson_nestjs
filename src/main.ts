dfeec4eeaa3f3aac439da3767560d76e8a02a691
roman <usankin.roma@gmail.com>
2023-06-16 01:35:52 +0400

    add validation, userModelDto

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './modules/app/app.module';
 import {ConfigService} from "@nestjs/config";
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService)
     const port = configService.get("port");
+    app.useGlobalPipes(new ValidationPipe())//Валидация
 
     // напсиание документации swagger
     const config = new DocumentBuilder()
