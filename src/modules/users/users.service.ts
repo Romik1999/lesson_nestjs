a6140011d01a359a5cc1036f897840598a370c03
Roman <usankin.roma@gmail.com>
2023-06-17 19:46:16 +0400

    add errors, added user uniqueness

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,8 +1,9 @@
-import {Injectable} from '@nestjs/common';
+import {BadRequestException, Injectable} from '@nestjs/common';
 import {InjectModel} from "@nestjs/sequelize";
 import {User} from "./models/user.model";
 import * as bcrypt from 'bcrypt';
 import {CreateUserDTO} from "./dto";
+import {AppError} from "../../common/errors";
 
 @Injectable()
 export class UsersService {
@@ -10,12 +11,23 @@ export class UsersService {
     }
 
     async hashPassword(password) {
-        return bcrypt.hash(password, 10)
+        return bcrypt.hash(password, 10);
     }
 
-    async createUser(dto): Promise<CreateUserDTO> {
-        dto.password = await this.hashPassword(dto.password)
-        await this.userRepository.create(dto)
+    async findUserByEmail(email: string) {
+        return this.userRepository.findOne({where: {email: email}});
+    }
+
+    async createUser(dto: CreateUserDTO): Promise<CreateUserDTO> {
+        const existUser = await this.findUserByEmail(dto.email)
+        if (existUser) throw new BadRequestException(AppError.USER_EXIST)
+        dto.password = await this.hashPassword(dto.password);
+        await this.userRepository.create({
+            firstName: dto.firstName,
+            userName: dto.userName,
+            email: dto.email,
+            password: dto.password
+        })
         return dto
     }
 }
